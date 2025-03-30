26f9e7ff6b94e940591e587ab110e2b9bc06e819
Rahul <gamerrahul1122@gmail.com>
2025-03-30 15:37:17 +0530

    Initial Commit

---
+++ ClipboardFlow/ClipboardFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardFlowApp.swift
+//  ClipboardFlow
+//
+//  Created by Rahul on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipboardFlow
+//
+//  Created by Rahul on 30/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
